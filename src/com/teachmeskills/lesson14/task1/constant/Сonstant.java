62bc74ee99d93af139eb1e2dff47f6517b2f1eaf
IvanFLINT <rpkdevil@gmail.com>
2024-02-24 19:59:51 +0300

    Lesson_14

--- src/com/teachmeskills/lesson14/task1/constant/Сonstant.java
+++ src/com/teachmeskills/lesson14/task1/constant/Сonstant.java
@@ -4,7 +4,7 @@ public interface Сonstant {
     String DOC_NUM = "docnum" + "\\w{9}";
     String CONTRACT = "contract" + "\\w{7}";
     String PATH = ".\\file\\source\\documentation.txt";
-    String DOC = ".\\file\\documents\\documentt.txt";
-    String CON = ".\\file\\documents\\contractt.txt";
+    String DOC = ".\\file\\documents\\document.txt";
+    String CON = ".\\file\\documents\\contract.txt";
     String ERROR_LOG = ".\\file\\logs\\error_log.txt";
 }
--- src/com/teachmeskills/lesson14/task1/service/Service.java
+++ src/com/teachmeskills/lesson14/task1/service/Service.java
@@ -12,58 +12,45 @@ public class Service {
             br = new BufferedReader(new FileReader(dir));
         } catch (FileNotFoundException e) {
             System.out.println("File does not exist");
-        }
-        String line = null;
-        while (true) {
-            try {
-                if (!((line = br.readLine()) != null)) break;
-            } catch (IOException e) {
-                System.out.println("The file is unreadable");
-                FileWriter writer = null;
-                try {
-                    writer = new FileWriter(Сonstant.ERROR_LOG, true);
-                } catch (IOException ex) {
-                    System.out.println("Write error");
-                }
-                try {
-                    writer.write("The file is unreadable" + "\n");
-                } catch (IOException ex) {
-                    System.out.println("Write error");
-                }
+            try (FileWriter writer = new FileWriter(Сonstant.ERROR_LOG, true)) {
+                writer.write("File does not exist" + "\n");
+            } catch (IOException ex) {
+                System.out.println("Write error");
             }
-            if (line.matches(Сonstant.DOC_NUM)) {
-                try (FileWriter writer = new FileWriter(Сonstant.DOC, true)) {
-                    writer.write("Valid document number: " + line + "\n");
-                } catch (IOException e) {
-                    System.out.println("Write error");
-                    FileWriter writer = null;
-                    try {
-                        writer = new FileWriter(Сonstant.ERROR_LOG, true);
-                    } catch (IOException ex) {
+            String line = null;
+            while (true) {
+                try {
+                    if (!((line = br.readLine()) != null)) break;
+                } catch (IOException ex1) {
+                    System.out.println("The file is unreadable");
+                    try (FileWriter writer = new FileWriter(Сonstant.ERROR_LOG, true)) {
+                        writer.write("The file is unreadable" + "\n");
+                    } catch (IOException ex2) {
                         System.out.println("Write error");
                     }
-                    try {
-                        writer.write("Write error" + "\n");
-                    } catch (IOException ex) {
-                        System.out.println("Write logs error");
+                }
+                if (line.matches(Сonstant.DOC_NUM)) {
+                    try (FileWriter writer = new FileWriter(Сonstant.DOC, true)) {
+                        writer.write("Valid document number: " + line + "\n");
+                    } catch (IOException ex1) {
+                        System.out.println("Write error");
+                        try (FileWriter writer = new FileWriter(Сonstant.ERROR_LOG, true)) {
+                            writer.write("Write error" + "\n");
+                        } catch (IOException ex2) {
+                            System.out.println("Write error");
+                        }
                     }
                 }
-            }
-            if (line.matches(Сonstant.CONTRACT)) {
-                try (FileWriter writer1 = new FileWriter(Сonstant.CON, true)) {
-                    writer1.write("Valid contract number: " + line + "\n");
-                } catch (IOException e) {
-                    System.out.println("Write error");
-                    FileWriter writer = null;
-                    try {
-                        writer = new FileWriter(Сonstant.ERROR_LOG, true);
-                    } catch (IOException ex) {
+                if (line.matches(Сonstant.CONTRACT)) {
+                    try (FileWriter writer1 = new FileWriter(Сonstant.CON, true)) {
+                        writer1.write("Valid contract number: " + line + "\n");
+                    } catch (IOException ex1) {
                         System.out.println("Write error");
-                    }
-                    try {
-                        writer.write("Write error" + "\n");
-                    } catch (IOException ex) {
-                        System.out.println("Write logs error");
+                        try (FileWriter writer = new FileWriter(Сonstant.ERROR_LOG, true)) {
+                            writer.write("Write error" + "\n");
+                        } catch (IOException ex2) {
+                            System.out.println("Write error");
+                        }
                     }
                 }
             }
