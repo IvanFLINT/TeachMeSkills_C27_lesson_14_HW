b167acc7116083bb85be2b080f2974fad538435b
IvanFLINT <rpkdevil@gmail.com>
2024-02-25 19:01:55 +0300

    Lesson_14

--- src/com/teachmeskills/lesson14/task1/runner/Runner.java
+++ src/com/teachmeskills/lesson14/task1/runner/Runner.java
@@ -19,6 +19,8 @@ public class Runner {
             Validator.validator(path);
         } catch (Exception e) {
             System.out.println("File path not found");
+        }finally {
+            sc.close();
         }
     }
 }
